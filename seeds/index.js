3bfc7e7ceb43d982da941e8075c6b0e2066fb3fe
Cavendishh <kavanderjanne@gmail.com>
2021-01-21 19:14:07 +0200

    Adding img functionality

--- seeds/index.js
+++ seeds/index.js
@@ -26,9 +26,23 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000)
     const price = Math.floor(Math.random() * 20) + 10
     const camp = new Campground({
+      author: '60083c25c225f02e180ff731',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251/1600x900',
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dgntv9bgo/image/upload/v1611248809/YelpCamp/mfyxvd0tunw1qic0puje.jpg',
+          filename: 'YelpCamp/mfyxvd0tunw1qic0puje'
+        },
+        {
+          url: 'https://res.cloudinary.com/dgntv9bgo/image/upload/v1611248816/YelpCamp/g6nlqjond6orgrjn6z9e.jpg',
+          filename: 'YelpCamp/g6nlqjond6orgrjn6z9e'
+        },
+        {
+          url: 'https://res.cloudinary.com/dgntv9bgo/image/upload/v1611248828/YelpCamp/wddod7rpl1inosa5jrh7.jpg',
+          filename: 'YelpCamp/wddod7rpl1inosa5jrh7'
+        }
+      ],
       description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium accusantium, veniam omnis non fuga dignissimos eaque tempora ipsam accusamus earum sunt magnam quisquam assumenda incidunt quod perspiciatis fugiat. Incidunt, repellendus!`,
       price
     })
@@ -36,7 +50,11 @@ const seedDB = async () => {
   }
 }
 
-seedDB().then(() => {
-  mongoose.connection.close()
-  console.log('Database closed')
+
+
+
+seedDB()
+  .then(() => {
+    mongoose.connection.close()
+    console.log('Database closed')
 })
